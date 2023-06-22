89b30e4af612019e97264db77a2759201234c4a2
m.petrenko <mandarinkashusha@gmail.com>
2023-06-22 15:00:23 +0300

    scrolll

---
+++ size/js/script.js
@@ -0,0 +1,7 @@
+//Кол-во прокпучених пикселей
+
+/*const windowScrollTop = window.pageYOffset;
+const windowScrollLeft = window.pageXOffset;
+        
+console.log(windowScrollTop);
+console.log(windowScrollLeft);*/

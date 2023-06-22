55a73204cba5e3ad5019d5e1f7381902c237f3e0
m.petrenko <mandarinkashusha@gmail.com>
2023-06-22 17:26:37 +0300

    scrollTop

--- size/js/script.js
+++ size/js/script.js
@@ -6,3 +6,18 @@ const windowScrollLeft = window.pageXOffset;
 console.log(windowScrollTop);
 console.log(windowScrollLeft);*/
 
+const block = document.querySelector('.lesson__block');
+
+block.scrollTop = 50;
+
+const elementClientWidth = block.clientWidth;
+const elementClientHeight = block.clientHeight;
+
+const elementScrollLeft = block.scrollLeft;
+const elementScrollTop = block.scrollTop;
+
+
+console.log(elementClientWidth);
+console.log(elementClientHeight);
+console.log(elementScrollLeft);
+console.log(elementScrollTop);
